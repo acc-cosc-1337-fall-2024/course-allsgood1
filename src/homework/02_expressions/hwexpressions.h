fcb8d81bf216f72aef2735b475e6ae09f88076e2
allsgood1 <59664235+allsgood1@users.noreply.github.com>
2024-09-15 02:01:28 +0000

    Homework 2

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,12 @@
 ﻿//example
 int add_numbers(int num1, int num2);
+double const tax_rate = .0675; 
 
 //write function prototype here
+int get_sales_tax_amount(double meal_amount) {
+    
+};
 
+int get_tip_amount(double meal_amount, double tip_rate) {
 
-
-
+};
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,35 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include<iostream>
 
 //write namespace using statement for cout
+using std::cout; using std::cin;
+
+double meal_amount;
+double tip_rate;
+double tip_amount;
+double tax_amount;
+double total;
 
 int main()
 {
+	cout<<"How much was your meal tonight? ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"What percentage would you like to tip? ";
+	cin>>tip_rate;
+
+	tip_rate = tip_rate *.01; //turning the input into a percentage
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
 	
+	total = tip_amount + tax_amount + meal_amount;
+	
+	return total;
+
+	cout<<"Your total is "<<total;
 
 	return 0;
 }
