bc6ba99343bf4b841ebd7cece9ed2928462262a5
allsgood1 <59664235+allsgood1@users.noreply.github.com>
2024-09-22 15:17:11 +0000

    Not getting the output to the screen; I am defeated

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,55 @@
 //write include statement for decisions header
+#include "decisions.h"
+
+
 
 
 //Write code for function(s) code here
+
+int get_letter_grade_using_if(int grade) {
+    char letter_Grade;
+    if (grade <= 100 && grade >= 90){
+        char letter_Grade = 'A';
+        return letter_Grade;
+    }   else if (grade>80){
+            char letter_Grade = 'B';
+            return letter_Grade;
+    }   else if (grade>70){
+            char letter_Grade = 'C';
+            return letter_Grade;
+    }   else if (grade>60){
+            char letter_Grade = 'D';
+            return letter_Grade;
+    }   else {
+        char letter_Grade = 'F';
+        return letter_Grade;
+    };
+    return letter_Grade;  
+}
+
+int get_letter_grade_using_switch(int grade) {
+    char letter_Grade;
+    int new_Grade = grade /10;
+    switch (new_Grade) {
+    case 9:
+        letter_Grade = 'A';
+        return letter_Grade;
+        break;
+    case 8:
+        letter_Grade = 'B';
+        return letter_Grade;
+        break;
+    case 7:
+        letter_Grade = 'C';
+        return letter_Grade;
+        break;
+    case 6:
+        letter_Grade = 'D';
+        return letter_Grade;
+        break;
+    default:
+        letter_Grade = 'F';
+        return letter_Grade;
+        break;
+    }
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,21 @@
 //write include statements
+#include "decisions.h"
+#include <iostream>
+
+using std::cout; using std::cin;
+using std::cout; using std::cin;
+int grade;
+int letter_Grade;
+
 
 int main() 
 {
-	return 0;
+	cout<<"What number grade did you recieve? ";
+	cin>>grade; 
+	int letter_Grade = get_letter_grade_using_if(grade);
+	cout<<letter_Grade;
+	cout<<"Let's try again, what grade did you recieve?";
+	cin>>grade;
+	int letter_Grade = get_letter_grade_using_switch(grade);
+	cout<<letter_Grade;
 }
